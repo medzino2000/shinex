e0226a463d77334258142c99b28ac9ff6ae64200
med zino <99444055+medzino2000@users.noreply.github.com>
2022-09-09 09:41:30 +0100

    Add files via upload

---
+++ build/precache-manifest.37dc92ab24766f0454836cfb7438ecfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93de9f9b983a852c0471",
+    "url": "/static/css/main.40ae7ed6.chunk.css"
+  },
+  {
+    "revision": "93de9f9b983a852c0471",
+    "url": "/static/js/main.93de9f9b.chunk.js"
+  },
+  {
+    "revision": "a28182bdbabbcdda0918",
+    "url": "/static/js/1.a28182bd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6351c1977f2f2a8f88a4bd96711c4711",
+    "url": "/index.html"
+  }
+];
